4e7b1e30c8cc0a7f567217e67b306e9d394a430b
Danish-Belal <danishexplore019@gmail.com>
2022-07-03 00:45:17 +0530

    Variables

---
+++ Start/JS/Variable.js
@@ -0,0 +1,16 @@
+var name = "Danish";
+//console.log(name);
+
+var name = "Zishan";
+console.log(name);
+
+let n = "Danish";
+console.log(n);
+
+// let n = "Zishan";  // can not redeclare block- scope.
+
+var A = 10;
+console.log(A);
+
+var _name = 'd';  // bad way to declare variable , with _ constructor starts
+console.log(_name);
