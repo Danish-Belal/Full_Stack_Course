d929f9650da3d5b3e7e87d529cee883e3e948947
Danish-Belal <danishexplore019@gmail.com>
2022-07-06 13:22:38 +0530

    Objects

---
+++ Start/JS/Objexts.js
@@ -0,0 +1,35 @@
+var student = {
+     Name : "Danish",
+     roll : 12,
+     marks : 10
+}
+
+
+console.log(student.Name);
+student.age = 21;
+
+console.log(student);
+
+
+// Bracktes.
+console.log(student["Name"]);
+
+function printPropertyr(obj , prop){
+     console.log(obj[prop]);  // Here we can not use dot notation.
+}
+
+printPropertyr(student , "age");
+
+// Deleting an property.
+console.log(student);
+console.log(delete student.age); 
+console.log(student);
+
+var student2 = student;
+console.log(student2);
+console.log(student===student2);
+console.log(student==student2);
+
+var student3 = new Object();
+console.log(student3);
+console.log(student===student3);
