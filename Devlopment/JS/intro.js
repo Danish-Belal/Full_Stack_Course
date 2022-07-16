33267d2d4071f965d7a676ec46a7a0530c9945c1
Danish-Belal <danishexplore019@gmail.com>
2022-07-16 20:18:34 +0530

    INto

---
+++ Devlopment/JS/intro.js
@@ -0,0 +1,20 @@
+var name = "Danish";
+var print = `Hello my name is  ${name} . `;
+console.log(`Hello my name is ${name}`);
+
+console.log(print);
+
+for (var index = 0; index <10; index++) {
+    console.log(index);
+    let val = 10;
+     
+}
+
+console.log(`hello`+ ++index);
+//console.log(val);
+
+let i = 0;
+while (i < 3) { // shows 0, then 1, then 2
+  alert( i );
+  i++;
+}
