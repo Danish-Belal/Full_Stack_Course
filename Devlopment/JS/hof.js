50a350a6e9231cc8ce1e3ff0e2c8924a91b0a144
Danish-Belal <danishexplore019@gmail.com>
2022-07-18 17:36:03 +0530

    Higher order Programming

--- Devlopment/JS/hof.js
+++ Devlopment/JS/hof.js
@@ -2,118 +2,118 @@
 
 // Functional Programming.
 // Better way
-//  var arr = [2,5,7,12]
-// let area = function(side){
-//      return side*side;
-// }
-// let perimeter = function(side){
-//      return side*4;
-// }
+ var arr = [2,5,7,12]
+let area = function(side){
+     return side*side;
+}
+let perimeter = function(side){
+     return side*4;
+}
 
-// let diagnole = function(side){
-//      return Math.sqrt(2)*side;
-// }
+let diagnole = function(side){
+     return Math.sqrt(2)*side;
+}
 
-// let calculate = function(arr , logic){
-//      let res= [];
-//      for(let i =0 ; i<arr.length ; i++){
-//           res.push(logic(arr[i]));
-//      }
-//      return res;
-// }
+let calculate = function(arr , logic){
+     let res= [];
+     for(let i =0 ; i<arr.length ; i++){
+          res.push(logic(arr[i]));
+     }
+     return res;
+}
 
-// console.log(calculate(arr , area));
-// console.log(calculate(arr , perimeter));
-//console.log(calculate(arr , diagnole));
+console.log(calculate(arr , area));
+console.log(calculate(arr , perimeter));
+console.log(calculate(arr , diagnole));
 
 
 // map.
-//Syntax -> arr.map(function(ele)){
+// Syntax -> arr.map(function(ele)){
 //      return {logic}
 // })
 
-// result arr // hof   // cb function.
+//result arr // hof   // cb function.
 
-// var areaofSquareArr = arr.map((num) => {
-//      return num*num;
-// })
+var areaofSquareArr = arr.map((num) => {
+     return num*num;
+})
 
 // 1) Each element of arr is traversed , it is availaible 
 //  in cd functional programing.
 // 2) we performe some action on that element.
 // 3) on writting return res is pushed inside an array
 
-//console.log(areaofSquareArr);
+console.log(areaofSquareArr);
 
-// var areaofSquareArr = arr.map(area);
-// console.log(areaofSquareArr);
+var areaofSquareArr = arr.map(area);
+console.log(areaofSquareArr);
 
-// // calculate (arr , logic); //-> convert this into map.
-// // arr.map(area).
+// calculate (arr , logic); //-> convert this into map.
+// arr.map(area).
 
-// // this is what we required.
-// // arr.calculate.
+// this is what we required.
+// arr.calculate.
 
-// Array.prototype.calculate = function(logic){
-//      let res = [];
-//      for(let i =0  ;i<this.length ; i++){
-//           res.push(logic(this[i]));
-//      }
-//      return res;
-// }
+Array.prototype.calculate = function(logic){
+     let res = [];
+     for(let i =0  ;i<this.length ; i++){
+          res.push(logic(this[i]));
+     }
+     return res;
+}
 
-// let narr = [2,5,7,12];
-// var ans= narr.calculate(area);
-// console.log(ans);
+let narr = [2,5,7,12];
+var ans= narr.calculate(area);
+console.log(ans);
 
-// // implementing own push method in arr.
-// Array.prototype.myPush = function(num){
-//      let len = this.length;
-//      this[len] = num;
-//      return this;
-// }
-// narr.myPush(45);
-// console.log(narr);
+// implementing own push method in arr.
+Array.prototype.myPush = function(num){
+     let len = this.length;
+     this[len] = num;
+     return this;
+}
+narr.myPush(45);
+console.log(narr);
 
-// console.log(narr.length);
-// narr[10] = 5;
-// console.log(narr.length);
-// narr.myPush(23);
-// console.log(narr);
-
-
-// // Filter.
-// function greater(num){
-//      if(num > 4) return num;
-// }
-
-// let Ans = narr.filter(greater);
-// console.log(Ans);
+console.log(narr.length);
+narr[10] = 5;
+console.log(narr.length);
+narr.myPush(23);
+console.log(narr);
 
 
-// var ar  = [10,12,23,45,67];
+// Filter.
+function greater(num){
+     if(num > 4) return num;
+}
 
-// // Sum of arr;
+let Ans = narr.filter(greater);
+console.log(Ans);
 
-// function sumOfArr(acc , curr){
-//      acc += curr;
-//      return acc;
-// }
 
-// var sum = ar.reduce(sumOfArr);
-// console.log(sum);
+var ar  = [10,12,23,45,67];
 
-// // find greater.
-// function largestVal(acc , curr){
-//      if( curr > acc) {
-//           acc = curr;
-//      }
-//      return acc;
-// }
+// Sum of arr;
 
-// var ans = ar.reduce(largestVal );
-// //var ans = ar.reduce(largestVal  , 0 ); // 0 is initial value of accumulator .
-// console.log(ans);
+function sumOfArr(acc , curr){
+     acc += curr;
+     return acc;
+}
+
+var sum = ar.reduce(sumOfArr);
+console.log(sum);
+
+// find greater.
+function largestVal(acc , curr){
+     if( curr > acc) {
+          acc = curr;
+     }
+     return acc;
+}
+
+var ans = ar.reduce(largestVal );
+//var ans = ar.reduce(largestVal  , 0 ); // 0 is initial value of accumulator .
+console.log(ans);
 
 
 const worldCapitals = [
