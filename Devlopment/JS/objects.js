d244008bbdf0110d2f1c6c202c98424972ffc565
Danish-Belal <danishexplore019@gmail.com>
2022-08-29 21:59:02 +0530

    Object in JS

---
+++ Devlopment/JS/objects.js
@@ -0,0 +1,29 @@
+var obj = {
+     name: "Danish",
+     roll : "31"
+};
+console.log(obj['name']);
+
+
+// Arrow function
+
+var add = (a,b) =>{
+     return a+b;
+}
+console.log(add(2,3));
+
+var sub = (a,b) => a-b;
+console.log(sub(2,3));
+
+var pName = "M1";
+var modal = 'note 8 pro';
+var price = 15000;
+
+
+// it will evaluate the value of the variable.
+var obj = {
+     pName ,
+     modal ,
+     price
+}
+console.log(obj);
