fea24f051bd8cf7dab65b498ea763c41a568449b
Danish-Belal <danishexplore019@gmail.com>
2022-09-01 23:32:19 +0530

    Scoping

---
+++ Devlopment/JS/Scoping.js
@@ -0,0 +1,5 @@
+greet();  // on window object this will print undefined.
+function greet(){
+     console.log('Hello form Danish');
+}
+greet();  // on window object this will print Hello from Danish.   do using execution context.
