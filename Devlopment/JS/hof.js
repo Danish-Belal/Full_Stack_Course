8eaf67df9609223a0dc0c80dec4d0a7adf7ba64b
Danish-Belal <danishexplore019@gmail.com>
2022-09-09 20:55:37 +0530

    Higher Order Function

--- Devlopment/JS/hof.js
+++ Devlopment/JS/hof.js
@@ -34,167 +34,45 @@ let area = function(side){
 
 //result arr // hof   // cb function.
 
-var areaofSquareArr = arr.map((num) => {
-     return num*num;
-})
+// var areaofSquareArr = arr.map((num) => {
+//      return num*num;
+// })
 
 // 1) Each element of arr is traversed , it is availaible 
 //  in cd functional programing.
 // 2) we performe some action on that element.
 // 3) on writting return res is pushed inside an array
 
-console.log(areaofSquareArr);
+//console.log(areaofSquareArr);
 
-var areaofSquareArr = arr.map(area);
-console.log(areaofSquareArr);
+// var areaofSquareArr = arr.map(area);
+// calculate(arr , logic);  // -> convert this to map function.
+// arr.calculate(logic);
+// console.log(areaofSquareArr);
 
-// calculate (arr , logic); //-> convert this into map.
-// arr.map(area).
+// Implementation of Map function.
+Array.prototype.calculate  = function(logic){
+     let res= [];
+     for(let i =0 ; i<this.length ;  i++){
+          res.push(logic(this[i]));
+     }
+     return res;
+}
 
-// //this is what we required.
-// arr.calculate.
+let newarr = [2,5,7,12];                // this points to newarr
+var ans = newarr.calculate(area);
+console.log(ans);
 
-// Array.prototype.calculate = function(logic){
-//      let res = [];
-//      for(let i =0  ;i<this.length ; i++){
-//           res.push(logic(this[i]));
-//      }
-//      return res;
-// }
+newarr.push(45);
+console.log(newarr);
 
-// let narr = [2,5,7,12];
-// var ans= narr.calculate(area);
-// console.log(ans);
-
-// // implementing own push method in arr.
-// Array.prototype.myPush = function(num){
-//      let len = this.length;
-//      this[len] = num;
-//      return this;
-// }
-// narr.myPush(45);
-// console.log(narr);
-
-// console.log(narr.length);
-// narr[10] = 5;
-// console.log(narr.length);
-// narr.myPush(23);
-// console.log(narr);
+// Implemetn oue own push function.
+Array.prototype.mypush = function(ele){
+     this[this.length] = ele;
+     return this;
+} 
+newarr.mypush(123);
+console.log(newarr);
 
 
-// // Filter.
-// function greater(num){
-//      if(num > 4) return num;
-// }
-
-// let Ans = narr.filter(greater);
-// console.log(Ans);
-
-
-// var ar  = [10,12,23,45,67];
-
-// // Sum of arr;
-
-// function sumOfArr(acc , curr){
-//      acc += curr;
-//      return acc;
-// }
-
-// var sum = ar.reduce(sumOfArr);
-// console.log(sum);
-
-// // find greater.
-// function largestVal(acc , curr){
-//      if( curr > acc) {
-//           acc = curr;
-//      }
-//      return acc;
-// }
-
-// var ans = ar.reduce(largestVal );
-// //var ans = ar.reduce(largestVal  , 0 ); // 0 is initial value of accumulator .
-// console.log(ans);
-
-
-// const worldCapitals = [
-//      {
-//        country: "India",
-//        capital: "Delhi",
-//        population: "2cr",
-//      },
-//      {
-//        country: "America",
-//        capital: "Washington DC",
-//        population: "6cr",
-//      },
-//      {
-//        country: "France",
-//        capital: "Paris",
-//        population: "2cr",
-//      },
-   
-//      {
-//        country: "England",
-//        capital: "London",
-//        population: "4cr",
-//      },
-//      {
-//        country: "Germany",
-//        capital: "Berlin",
-//        population: "2cr",
-//      },
-//    ];
-   
-
-// // 01 Print country name and their capital // desired output -> India,Delhi.
-
-// // 02 Return the number of countries with a particular population .
-// //** Desired output -> { 2cr : 3 , 4cr ; 1 , 6cr:1}.
-
-// // 3 get country name with population less than 5cr.
-// // Desired output [India , France , England , Germeny]
-
-
-// // 01
-// for(cap in worldCapitals){
-//      console.log(worldCapitals[cap].country+ " -> "+ worldCapitals[cap].capital);
-// }
-
-
-
-
-// // 02.
-
-
-// // 03.
-// var res = [];
-// worldCapitals.forEach(element => {
-//      if(element.population < "5cr"){
-//           res.push(element.country);
-//      }
-// });
-// console.log(res);
-
-// function  getPopulationLessThan5cr(obj){
-//      if(obj.population < "5cr"){
-//           return true;
-//      }
-// }
-// function getCountry(obj){
-//      return obj.country;
-// }
-// var res= worldCapitals.filter(getPopulationLessThan5cr).map(getCountry);
-// console.log(res);
-
-// // using reduce.
-
-// function getLessThan5(arr,obj){
-//      if(obj.population < "5cr"){
-//           arr.push(obj.country);
-//      }
-//      return arr; 
-// }
-// var res = worldCapitals.reduce( getLessThan5, [] );
-// console.log(res);
-
-
+// polyfills ->  adding a function to a class which is not present in your browser .
