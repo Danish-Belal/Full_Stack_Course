4ea58ae99db73436cdf004d9a0bf7d314e6d1d1d
Danish-Belal <danishexplore019@gmail.com>
2022-09-17 16:14:49 +0530

    Call , Apply , Bind in JS

--- Devlopment/JS/call_apply_bind.js
+++ Devlopment/JS/call_apply_bind.js
@@ -4,43 +4,43 @@
 // syntx :-> function_name.call(obj_to_which_this_will_point). 
 
 
-// let crickter1 = {
-//      fname : "Kapil",
-//      lname : "Dev",
-//      welcomwfun  : function(){
-//           console.log(`welcome ${this.fname} -> ${this.lname}`);
-//      }
-// }
+let crickter1 = {
+     fname : "Kapil",
+     lname : "Dev",
+     welcomwfun  : function(){
+          console.log(`welcome ${this.fname} -> ${this.lname}`);
+     }
+}
 
 // // function borrow.
 // // Calls method of an object , subsitutuing another object for current object . this shoud point current obj.
 // crickter1.welcomwfun.call(crickter2);   // if there is a function in an obj and we want to access that function for another object.
 
 
-// let crickter2 = {
-//      fname : "Ravi",
-//      lname : "Sastri"
-// }
+let crickter2 = {
+     fname : "Ravi",
+     lname : "Sastri"
+}
 
-// let welcome = function(){
-//      console.log(`Hello ${this.fname}  ${this.lname}`);
-// }
-// welcome.call(crickter1);   
+let welcome = function(){
+     console.log(`Hello ${this.fname}  ${this.lname}`);
+}
+welcome.call(crickter1);   
 // welcome.call(crickter2);
 
 
 
-// let welcome2 = function(email , M_no , location){
-//      console.log(`Hello ${this.fname}  ${this.lname},
-//                     Thanks for signing! your email is ${email}
-//                     Your Mno is ${M_no},
-//                     Your location is ${location}`);
-// }
+let welcome2 = function(email , M_no , location){
+     console.log(`Hello ${this.fname}  ${this.lname},
+                    Thanks for signing! your email is ${email}
+                    Your Mno is ${M_no},
+                    Your location is ${location}`);
+}
 
-// welcome2.call(crickter1 , "danish@gmail.com" , 9891600851 , "India");  // we can add addition info if function needed.
+ welcome2.call(crickter1 , "danish@gmail.com" , 9891600851 , "India");  // we can add addition info if function needed.
 
 // // apply method.
-// welcome2.apply(crickter2 , ["dani@xyz" , 989988 , "india"]);  // apply take array as  parameter.
+ welcome2.apply(crickter2 , ["dani@xyz" , 989988 , "india"]);  // apply take array as  parameter.
 
 
 // Binnd Method.
@@ -57,17 +57,17 @@ function carDescription(date , INR){
 }
 
 // carDescription(car);
-// let bindfn = carDescription.bind(car);
-// bindfn();
+let bindfn = carDescription.bind(car);
+bindfn();
 
 
 // Implementing our own bind method.
-//  Function.prototype.myBind = function(args){
-//      let funBind = this;
-//      return function(){
-//           funBind.call(args);
-//      }
-//  }
+ Function.prototype.myBind = function(args){
+     let funBind = this;
+     return function(){
+          funBind.call(args);
+     }
+ }
  // generic , if more arguments
  Function.prototype.myBind = function(...args){
      let funBind = this;
