bfaef23748ab2a2a0b582344ed1cef8aaf9977e9
Danish-Belal <danishexplore019@gmail.com>
2022-09-22 20:35:40 +0530

    Todo Application using react

---
+++ Devlopment/React/todo/src/Components/Todo.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+
+export default class Todo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tasks: ["Revise JS", "Revise DSA", "Operating System"],
+      currentTask: "",
+    };
+  }
+
+  handleTask = () => {
+    this.setState({
+      tasks: [...this.state.tasks, this.state.currentTask]
+    })
+  }
+
+  handleChange = (e) => {
+    console.log(e.target.value)
+    this.setState({
+      currentTask: e.target.value
+    })
+
+
+  }
+  render() {
+    return (
+      // <div>Todo</div>
+      <div>
+        <input type="text" placeholder='Enter Your Task' onChange={this.handleChange} />
+        <button onClick={this.handleTask}> Add</button>
+
+        {
+          this.state.tasks.map(task => {
+            return (
+              <li>
+                <p>{task}</p>
+                <button>Delete</button>
+              </li>
+            )
+          })
+
+        }
+      </div>
+
+    )
+  }
+}
