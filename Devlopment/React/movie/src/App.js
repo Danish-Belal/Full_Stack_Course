6a5cf3fc7181ddfbcdf281b760ea5b1603857976
Danish-Belal <danishexplore019@gmail.com>
2022-10-04 23:28:12 +0530

    App JS

--- Devlopment/React/movie/src/App.js
+++ Devlopment/React/movie/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Navbar  from './Components/Navbar';
 import Banner from './Components/Banner';
-
+import List from './Components/List';
+import Faviourate from './Components/Faviourate';
 
 function App() {
   return (
     <div>
     <Navbar />
     <Banner />
+    <List />
+    <Faviourate />
     </div>
   );
 }
