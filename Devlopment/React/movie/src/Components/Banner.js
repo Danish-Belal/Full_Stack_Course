ce25a2decfec03699a1651ac3f2c7d36fc81a5c9
Danish-Belal <danishexplore019@gmail.com>
2022-10-09 23:22:03 +0530

    Movie App

--- Devlopment/React/movie/src/Components/Banner.js
+++ Devlopment/React/movie/src/Components/Banner.js
@@ -5,15 +5,22 @@ export default class Banner extends Component {
   render() {
     let movie = movies.results[0];
     return (
-      <div class="card">
-        <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} class="card-img-top banner-img" alt="..."/>
-          <div class="card-body">
-            <h5 class = "card-title banner-title">{movie.original_title}</h5>
-            <p class="card-text banner-text">{movie.overview}</p>
-          </div>
-      </div>
-
-
-    )
+      <>
+        {
+          movie.length === 0 ? (
+            <div className="spinner-border text-warning" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>)
+            : (
+              <div className="card">
+                <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} class="card-img-top banner-img" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title banner-title">{movie.original_title}</h5>
+                  <p className="card-text banner-text">{movie.overview}</p>
+                </div>
+              </div>
+            )}
+      </>
+    );
   }
 }
--- Devlopment/React/movie/src/Components/List.js
+++ Devlopment/React/movie/src/Components/List.js
@@ -2,30 +2,74 @@ import React, { Component } from 'react'
 import { movies } from './getMovies'
 
 export default class List extends Component {
+     constructor() {
+          super();
+          this.state = {
+               hover: '',
+          };
+     }
+
+     handleEnter = (id) => {
+          this.setState({
+               hover: id
+          })
+     }
+
+     handleLeave = () => {
+          this.setState({
+               hover: '',
+          })
+     }
      render() {
           let allMovies = movies.results;
           return (
-               <div>
-                    <h3 className='Tranding'>Trading</h3>
-                    <div className='movies-list'>
-                         {allMovies.map((movieObj) => {
-                              return (
-                                   <div class="card movie-card">
-                                        <img src={`https://image.tmdb.org/t/p/original/${movieObj.backdrop_path}`} class="card-img-top movie-img" alt="..." />
-                                        <div class="card-body">
-                                             <h5 class="card-title movie-title">{movieObj.original_title}</h5>
-                                             <div className='button-wrapper'>
-                                                  <a href="#" class="btn btn-primary movie-button">Add to Favourites</a>
-                                             </div>
-                                        </div>
+               <>
+                    {
+                         allMovies.length === 0 ? (
+                              <div class="spinner-border text-info" role="status">
+                                   <span class="visually-hidden">Loading...</span>
+                              </div>
+                         ) : (
+                              <div>
+                                   <h3 className='Tranding'>Trading</h3>
+                                   <div className='movies-list'>
+                                        {allMovies.map((movieObj) => {
+                                             return (
+                                                  <div class="card movie-card" onMouseEnter={() => this.handleEnter(movieObj.id)} onMouseLeave={this.handleLeave}>
+                                                       <img src={`https://image.tmdb.org/t/p/original/${movieObj.backdrop_path}`} class="card-img-top movie-img" alt="..." />
+                                                       <div class="card-body">
+                                                            <h5 class="card-title movie-title">{movieObj.original_title}</h5>
+
+                                                            <div className='button-wrapper'>
+                                                                 {this.state.hover == movieObj.id &&
+                                                                      <a href="#" class="btn btn-primary movie-button">Add to Favourites</a>
+                                                                 }
+                                                            </div>
+
+                                                       </div>
+                                                  </div>
+
+                                             )
+                                        }
+
+                                        )}
                                    </div>
+                              </div>
+                         )
+                    }
+                    <>
+                         <nav aria-label="Page navigation example" className='pagination'>
+                              <ul class="pagination">
+                                   <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                                   <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                   <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                   <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                   <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                              </ul>
+                         </nav>
+                    </>
+               </>
 
-                              )
-                         }
-
-                         )}
-                    </div>
-               </div>
-          )
+          );
      }
 }
