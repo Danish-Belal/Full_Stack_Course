591e426d00c906e2ded57e4ae762f051c6fc961f
Danish-Belal <danishexplore019@gmail.com>
2022-10-12 21:19:55 +0530

    Movie App

--- Devlopment/React/movie/src/Components/Faviourate.js
+++ Devlopment/React/movie/src/Components/Faviourate.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class Faviourate extends Component {
   render() {
     return (
-          <h1>Hello {this.props.name}</h1>
-      
+      <h1>Hello {this.props.name}</h1>
+
     )
   }
 }
